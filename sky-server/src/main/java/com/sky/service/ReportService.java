1eb3b9484a7f5a8c2e863a15572d83573a974f22
ItKongMu <1814225896@qq.com>
2024-11-14 22:22:01 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,17 +1,23 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
 public interface ReportService {
 
     /**
-     * 获取营业额统计数据
+     * 获取指定区间内的营业额统计数据
      * @return
      */
     TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
 
     TurnoverReportVO getTurnoverStatisticsDemo(LocalDate begin, LocalDate end);
 
+    /**
+     * 获取指定区间内的用户统计数据
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 }
